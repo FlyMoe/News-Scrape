3b54e2cd9af4a9d8d21a33e08b584ace8a33a23a
FlyMoe <rrgarcia@gmail.com>
2016-08-05 00:39:12 -0400

    Fix the next button, adding comments, and the delete section. All should be working

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(bodyParser.urlencoded({
 
 // Database configuration
 var mongojs = require('mongojs');
-//var databaseUrl = "news";
+var databaseUrl = "news";
 var articles = ["scrapedData"];
-var databaseUrl = "mongodb://heroku_zkwrthww:gp55nhv3shtgrb6ui7kad3piti@ds031865.mlab.com:31865/heroku_zkwrthww";
+//var databaseUrl = "mongodb://heroku_zkwrthww:gp55nhv3shtgrb6ui7kad3piti@ds031865.mlab.com:31865/heroku_zkwrthww";
 
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, articles);
@@ -34,6 +34,8 @@ db.on('error', function(err) {
   console.log('Database Error:', err);
 });
 
+// Set global counter variable that is used in moving to the next object
+counter=0;
 
 // Scrape the NYTimes articles
 app.get('/', function(req, res) {
@@ -50,17 +52,12 @@ app.get('/', function(req, res) {
 
 		// an empty array to save the data that we'll scrape
 		var result = [];
-		$('h2.story-heading').each(function(i, element){
-			//result.push($(this).text());
-		 	// save the request body as an object called scraped
-		 	var title = $(this).text().trim();
-		 	//var summary = $('p.summary').text().trim();
-			//var articles = {'title':title, 'summary':summary, comments:""};
-			var articles = {'title':title, comments:""};
-			//console.log("articles:" +articles.title);
-			  //console.log("summary:" +scraped.summary);
-			  // Insert scraped object, since it's already an object
 
+		$('h2.story-heading').each(function(i, element){
+			
+		 	var title = $(this).text().trim();
+			var articles = {'title':title, comments:""};
+			
 			// Check if title is missing. If it is, don't insert a blank title into
 			// the database.
 			if (title !== "") {
@@ -75,24 +72,25 @@ app.get('/', function(req, res) {
 		  		});
 			}		
 		});
-		// Find all articles and send the data over to handlebars
-		db.scrapedData.find({}, function(err,data)
+
+		// Find the article to display and send the data over to handlebars
+		db.scrapedData.find({}).limit(1).skip(counter, function(err,data)
 		{
 			res.render('index', {data});
-		})
+		});
 	});
 	
 });
-
 
 // ADD COMMENT
 app.post('/comment', function(req, res) {
 	// set the comment variable
 	var comment = req.body;
 	// update the database with the new comment
-	db.scrapedData.update({"_id": mongojs.ObjectId(comment.article_id)},  {$set: {'comments': comment.comment}}, function(err, docs) {
+	db.scrapedData.update({"_id": mongojs.ObjectId(comment.article_id)}, {$set: {comments:comment.comment}}, function(err, docs) {
 		 // show any errors
 		if (err) {console.log(err);}
+		// Redirect to the index page
 		res.redirect('/');
 	});
 });
@@ -102,12 +100,25 @@ app.post('/delete', function(req, res) {
 	// set the delete_comment variable
 	var delete_comment = req.body;
 	// delete the comment from the database
-	db.scrapedData.update({"_id": mongojs.ObjectId(delete_comment.article_id)}, {$set: {'comments': delete_comment.comment}}, function(err, docs) {
+	//db.scrapedData.update({"_id": mongojs.ObjectId(delete_comment.article_id)}, {$set: {comments:""}}, function(err, docs) {
+	db.scrapedData.update({"_id": mongojs.ObjectId(delete_comment.article_id)}, {$set: {comments:""}}, function(err, docs) {
 		if (err) {console.log(err);}
+		// Redirect to the index page
 		res.redirect('/');
 	});
 });
 
+
+// MOVE TO THE NEXT OBJECT
+app.get('/next', function(req, res) {	
+	// Upgrade the counter
+	counter++;
+	// Move to the next object
+	db.scrapedData.find({}).limit(1).skip(counter, function(err,data) {
+		if (err) {console.log(err);}
+		res.render('index', {data});
+	});
+});
 
 // LISTENER
 app.listen(PORT, function() {
